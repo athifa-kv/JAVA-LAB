398562c63226ad8efe811d20f3f716189bfedbd3
athifa-kv <kvathifa@gmail.com>
2026-01-28 11:16:56 +0530

    Add files via upload

---
+++ MatrixAddition.java
@@ -0,0 +1,45 @@
+package athifa;
+import java.util.Scanner;
+public class MatrixAddition {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    	Scanner sc = new Scanner(System.in);
+    	int rows,cols;
+    	System.out.print("Enter number of rows: ");
+    	rows = sc.nextInt();
+    	System.out.print("Enter number of columns: ");
+    	cols = sc.nextInt();
+    	int[][] A = new int[rows][cols];
+    	int[][] B = new int[rows][cols];
+    	int[][] C = new int[rows][cols];
+    	// Read first matrix
+    	System.out.println("Enter elements of Matrix A: ");
+    	for (int i = 0; i < rows; i++) {
+    		for (int j = 0; j < cols; j++) {
+    			A[i][j] = sc.nextInt();
+    		}
+    	}
+    	// Read second matrix
+    	System.out.println("Enter elements of Matrix B: ");
+    	for (int i = 0; i < rows; i++) {
+    		for (int j = 0; j < cols; j++) {
+    			B[i][j] = sc.nextInt();
+   }
+}
+    	// matrix addition
+    	for (int i = 0; i < rows; i++) {
+    		for (int j = 0; j < cols; j++) {
+    			C[i][j] = A[i][j] + B[i][j];
+    		}
+    	}
+    	//Display result matrix
+    	System.out.println("Resultant Matrix (A + B): ");
+    	for (int i = 0; i < rows; i++) {
+    		for (int j = 0; j < cols; j++) {
+    			System.out.print(C[i][j] + "\t");
+    		}
+    		System.out.println();
+    	}
+    	sc.close();
+    }
+}
